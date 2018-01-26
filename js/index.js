0541db364c3c3c028599239421dcb0ebc701d826
dolphin666 <479019769zxx@gmail.com>
2018-01-26 16:50:23 +0800

    2.26  9:33am ceo

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 /******移动端头部按钮*******/
-$(document).ready(function(){
-    $("#m_menu_btn").click(function(){
-        if($(".m_menu_list").css("display")=="none"){
+$(document).ready(function () {
+    $("#m_menu_btn").click(function () {
+        if ($(".m_menu_list").css("display") == "none") {
             $(".m_menu_list").show();
             $("#mengban").show()
-        }else{
+        } else {
             $(".m_menu_list").hide();
             $("#mengban").hide()
         }
@@ -13,16 +13,16 @@ $(document).ready(function(){
 
 /******交易表格头部动效******/
 
-$(document).ready(function(){
+$(document).ready(function () {
     /*$(".swipe_menu div").mouseenter(function(){
         $(this).children("p").css("display","block");
     });*/
     // $(".swipe_menu div").mouseleave(function(){
     //     $(this).children("p").css("display","none");
     // });
-    $(".swipe_menu div").click(function(){
-        $(this).parentNode.children.children("p").css("display","none");
-        $(this).children("p").css("display","block");
+    $(".swipe_menu div a").click(function () {
+        $(this).parent().siblings().children("p").css("display", "none");
+        $(this).parent().children("p").css("display", "block");
     });
 });
 
@@ -44,12 +44,28 @@ $(document).ready(function () {
     });
 });
 /********** 手机端chart显示隐藏 **********/
-$(document).ready(function(){
-    $(".t_con_h").click(function(){
-        if($(this).siblings().css("display")=="none"){
-            $(this).next().css("display","block");
-        }else{
-            $(this).next().css("display","none");
+$(document).ready(function () {
+    $(".t_con_h li:nth-child(1)").click(function () {
+        if ($(this).parent().siblings().css("display") == "none") {
+            $(this).parent().next().css("display", "block");
+        } else {
+            $(this).parent().next().css("display", "none");
+        }
+    });
+
+    $(".t_con_h li:nth-child(2)").click(function () {
+        if ($(this).parent().siblings().css("display") == "none") {
+            $(this).parent().next().css("display", "block");
+        } else {
+            $(this).parent().next().css("display", "none");
+        }
+    });
+
+    $(".t_con_h li:nth-child(3)").click(function () {
+        if ($(this).parent().siblings().css("display") == "none") {
+            $(this).parent().next().css("display", "block");
+        } else {
+            $(this).parent().next().css("display", "none");
         }
     });
 });
